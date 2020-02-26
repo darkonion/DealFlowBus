5db6eca50f2648e9dba9146c6dba83a75296ba8c
darkonion <pawkapl89@gmail.com>
2020-02-26 01:00:13 +0100

    minorities

--- database-main-reader/src/main/java/com/dealflowbus/databasemainreader/repository/NoteRepository.java
+++ database-main-reader/src/main/java/com/dealflowbus/databasemainreader/repository/NoteRepository.java
@@ -1,9 +1,13 @@
 package com.dealflowbus.databasemainreader.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dealflowbus.databasemainreader.entities.Note;
 
 public interface NoteRepository extends JpaRepository<Note, Integer> {
+	
+	List<Note> findAllByOrderByIssueDateDesc();
 
 }
