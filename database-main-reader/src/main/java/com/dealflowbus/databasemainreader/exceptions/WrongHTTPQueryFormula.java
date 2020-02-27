cad6edc4d29e524b5668b14e4a2743a20628ed93
darkonion <pawkapl89@gmail.com>
2020-02-27 01:39:50 +0100

    Another exception

---
+++ database-main-reader/src/main/java/com/dealflowbus/databasemainreader/exceptions/WrongHTTPQueryFormula.java
@@ -0,0 +1,17 @@
+package com.dealflowbus.databasemainreader.exceptions;
+
+public class WrongHTTPQueryFormula extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7073847123116598939L;
+
+	public WrongHTTPQueryFormula(String message) {
+		super(message);
+		
+	}
+	
+	
+
+}
