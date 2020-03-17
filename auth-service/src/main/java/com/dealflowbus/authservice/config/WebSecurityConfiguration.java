cabf2f6bd90f93d7c11c7e13e2fb85b398f4d192
darkonion <pawkapl89@gmail.com>
2020-03-17 11:03:26 +0100

    google cloud working deploy

--- auth-service/src/main/java/com/dealflowbus/authservice/config/WebSecurityConfiguration.java
+++ auth-service/src/main/java/com/dealflowbus/authservice/config/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -31,6 +32,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+	    .authorizeRequests()
+	    .antMatchers("/manage/health").permitAll()
+	    .anyRequest().authenticated();
+		
 	
+	}
 	
 }
